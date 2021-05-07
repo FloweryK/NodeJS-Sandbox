1ee505a11225b23c5458bf5a12b94ef75ab04f08
Minsang <flowerk94@gmail.com>
2021-05-07 12:54:15 +0900

    24강까지 내용 추가

--- OpenTutorials-WEB2-Nodejs/main.js
+++ OpenTutorials-WEB2-Nodejs/main.js
@@ -5,17 +5,66 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var querydata = url.parse(_url, true).query;
-    if(_url === '/'){
-        _url = '/index.html';
+    var pathname = url.parse(_url, true).pathname;
+    var title = querydata.id;
+
+    console.log(`url: ${_url}`);
+
+    if(pathname === '/') {
+        fs.readFile(`data/${querydata.id}`, 'utf-8', function (err, description) {
+            if(querydata.id === undefined){
+                title = 'welcome';
+                description = 'Hello, Node.js'
+            }
+
+            fs.readdir('data/', function (error, filelist) {
+                /*
+                var list = `
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        <li><a href="/?id=HelloHTML">Hello HTML</a></li>
+                    </ol>`
+                 */
+
+                var list = '<ol>';
+                var i = 0;
+                while(i<filelist.length){
+                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>\n`
+                    i ++
+                }
+                list += '</ol>'
+
+                var template = `
+                    <!doctype html>
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>WEB - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    ${list}
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                    </body>
+                    </html>
+                    `;
+
+                response.writeHead(200);
+                response.end(template);
+            });
+
+        });
+    } else {
+      response.writeHead(404);
+      fs.readFile('src/not_found.html', 'utf-8', function (err, description){
+         response.end(description);
+      });
     }
-    if(_url === '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    response.end(querydata.id);
-    //response.end(fs.readFileSync(__dirname + _url));
 
 });
+
 app.listen(3000);
---
+++ OpenTutorials-WEB2-Nodejs/syntax/conditional.js
@@ -0,0 +1,8 @@
+var args = process.argv;
+
+if(args[2] === '1'){
+    console.log(`condition is true`);
+}
+else {
+    console.log(`condition is false`);
+}
